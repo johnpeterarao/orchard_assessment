05bad3caa572ee2f3e7ee025df84a3bcc0eea5ac
John Peter <johnpeterarao@gmail.com>
2025-03-19 01:16:34 +0800

    Scrolling animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,96 +9,17 @@ import { imageGridData } from "@/constants/section-image-grid";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ReactLenis } from "lenis/react";
+import { triggerAnimation } from "@/utils/scrollAnimation";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
-
-
-  const triggerAnimation = () => {
-    const ScrollTriggerSettings = {
-      trigger: ".product__section",
-      start: "top 30%",
-      toggleActions: "play reverse play reverse"
-    };
-
-    const leftXValues = [-250, -450, -200];
-    const rightXValues = [250, 450, 200];
-    const centerYValues = [-100, -50, -50];
-    const leftRotationValues = [-30, -20, -35];
-    const rightRotationValues = [30, 20, 35];
-    const yValues = [100, -150, -400];
-
-    const rows = gsap.utils.toArray('.grid__row') as HTMLDivElement[];
-
-    rows.forEach((row, index: number) => {
-      const leftColumn = row.querySelector('.grid__images--cover') as HTMLElement;
-      const rightColumn = row.querySelector('.grid__content') as HTMLElement;
-      const centerColumn = row.querySelector('.grid__images--items') as HTMLElement;
-
-      gsap.to(leftColumn, {
-        x: leftXValues[index],
-        scrollTrigger: {
-          trigger: ".grid__section",
-          start: "top 0",
-          end: "150% bottom",
-          scrub: true,
-          onUpdate: (self) => {
-            const progress = self.progress;
-            leftColumn.style.transform = `translate(${progress * leftXValues[index]}px, ${progress * yValues[index]}) rotate(${progress * leftRotationValues[index]}deg)`;
-            leftColumn.style.opacity = `${1 - (progress * 0.75)}`
-          }
-        }
-      });
-
-      gsap.to(rightColumn, {
-        x: rightXValues[index],
-        scrollTrigger: {
-          trigger: ".grid__section",
-          start: "top 0",
-          end: "150% bottom",
-          scrub: true,
-          onUpdate: (self) => {
-            const progress = self.progress;
-            rightColumn.style.transform = `translate(${progress * rightXValues[index]}px, ${progress * yValues[index]}) rotate(${progress * rightRotationValues[index]}deg)`;
-            rightColumn.style.opacity = `${1 - (progress * 0.75)}`
-          }
-        }
-      });
-
-      gsap.to(centerColumn, {
-        y: centerYValues[index],
-        scrollTrigger: {
-          trigger: ".grid__section",
-          start: "top 0",
-          end: "150% bottom",
-          scrub: true,
-          onUpdate: (self) => {
-            const progress = self.progress;
-            centerColumn.style.transform = `translateY(${progress * yValues[index]})`
-            centerColumn.style.opacity = `${1 - (progress * 0.75)}`
-          }
-        }
-      });
-
-    })
-
-    gsap.to('.product__card', {
-      y: 0,
-      stagger: 0.1,
-      duration: 0.5,
-      ease: "power1.out",
-      scrollTrigger: ScrollTriggerSettings
-    })
-  }
 
   useEffect(() => {
 
     triggerAnimation();
 
   }, [])
-
-  
 
   const handleConsoleLog = (e: React.MouseEvent<HTMLAnchorElement>) => {
     console.log('Element --> ', e.currentTarget);
---
+++ src/utils/scrollAnimation.tsx
@@ -0,0 +1,120 @@
+import gsap from "gsap";
+
+export const triggerAnimation = () => {
+    if (window.innerWidth <= 1024) return;
+
+    const leftXValues = [-250, -450, -200];
+    const rightXValues = [250, 450, 200];
+    const centerYValues = [-100, -50, -50];
+    const leftRotationValues = [-30, -20, -35];
+    const rightRotationValues = [30, 20, 35];
+    const yValues = [100, -150, -400];
+    // const ScrollTriggerSettings = {
+    //   trigger: ".product__section",
+    //   start: "top",
+    //   toggleActions: "play reverse play reverse"
+    // };
+    const rows = gsap.utils.toArray('.grid__row') as HTMLDivElement[];
+   
+
+    rows.forEach((row, index: number) => {
+      const leftColumn = row.querySelector('.grid__images--cover') as HTMLElement;
+      const rightColumn = row.querySelector('.grid__content') as HTMLElement;
+      const centerColumn = row.querySelector('.grid__images--items') as HTMLElement;
+
+      gsap.to(leftColumn, {
+        x: leftXValues[index],
+        scrollTrigger: {
+          trigger: ".grid__section",
+          start: "top 0",
+          end: "150% bottom",
+          scrub: true,
+          onUpdate: (self) => {
+            const progress = self.progress;
+            leftColumn.style.transform = `translate(${progress * leftXValues[index]}px, ${progress * yValues[index]}) rotate(${progress * leftRotationValues[index]}deg)`;
+            leftColumn.style.opacity = `${1 - (progress * 0.75)}`
+          }
+        }
+      });
+
+      gsap.to(rightColumn, {
+        x: rightXValues[index],
+        scrollTrigger: {
+          trigger: ".grid__section",
+          start: "top 0",
+          end: "150% bottom",
+          scrub: true,
+          onUpdate: (self) => {
+            const progress = self.progress;
+            rightColumn.style.transform = `translate(${progress * rightXValues[index]}px, ${progress * yValues[index]}) rotate(${progress * rightRotationValues[index]}deg)`;
+            rightColumn.style.opacity = `${1 - (progress * 0.75)}`
+          }
+        }
+      });
+
+      gsap.to(centerColumn, {
+        y: centerYValues[index],
+        scrollTrigger: {
+          trigger: ".grid__section",
+          start: "top 0",
+          end: "150% bottom",
+          scrub: true,
+          onUpdate: (self) => {
+            const progress = self.progress;
+            centerColumn.style.transform = `translateY(${progress * yValues[index]})`
+            centerColumn.style.opacity = `${1 - (progress * 0.75)}`
+          }
+        }
+      });
+
+    })
+
+    
+    // Get all product sections
+    const rows2 = gsap.utils.toArray('.product__section') as HTMLDivElement[];
+
+    
+    rows2.forEach((row) => {
+   
+        const cards = row.querySelectorAll('.col') as NodeListOf<HTMLElement>;
+
+        cards.forEach((card, cardIndex) => {
+            const yValues = [200, 150, 100];
+
+            card.style.opacity = `0`;
+            
+            gsap.from(card, {
+                y: 0,        
+                duration: 1,    
+                delay: cardIndex * 0.5,
+                stagger: 0.2,
+                scrollTrigger: {
+                    trigger: row,  
+                    start: "top bottom", 
+                    end: "bottom bottom", 
+                    scrub: true,  
+                    onUpdate: (self) => {
+                        const progress = self.progress;
+                        const progressPercentage = (progress * 100);
+                        const totalPX = yValues[cardIndex];
+                        const scrolledPX = (progressPercentage / 100) * totalPX;
+
+                        const totalPxs = totalPX - scrolledPX;
+
+                        card.style.transform = `translateY(-${ totalPxs < 0 ? 0 : totalPxs}px)`;
+                        
+                        card.style.opacity = `${self.progress}`
+                    }
+                }
+            });
+        });
+    });
+
+    // gsap.to('.product__card', {
+    //   y: 0,
+    //   stagger: 5,
+    //   duration: 12,
+    //   ease: "power1.out",
+    //   scrollTrigger: ScrollTriggerSettings
+    // })
+}
